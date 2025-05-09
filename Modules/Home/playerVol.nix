085b80a01e549acbc5511bd41d5d5c7b0cfc4872
Immelancholy <lenalowes0@gmail.com>
2025-05-09 23:11:36 +0100

    jghadlkjghald

--- Modules/Home/playerVol.nix
+++ Modules/Home/playerVol.nix
@@ -7,6 +7,7 @@
 }:
 with lib; let
   inherit (pkgs.stdenv.hostPlatform) system;
+  inherit self;
   cfg = config.player;
   playerVolMPD = self.packages.${system}.playerVolMPD;
   playerVolMpris = self.packages.${system}.playerVolMpris;
