b37ce01024a5d3f05ffdcdfa94d9fb025b25d588
Immelancholy <lenalowes0@gmail.com>
2025-05-09 23:06:57 +0100

    kjghladg

--- flake.nix
+++ flake.nix
@@ -10,6 +10,8 @@
     nixpkgs,
     ...
   }: let
+    inherit nixpkgs;
+    inherit self;
     systems = [
       "x86_64-linux"
       "aarch64-linux"
@@ -34,7 +36,7 @@
     formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
     homeManagerModules = rec {
-      all = import ./Modules/Home;
+      all = import ./Modules/Home self;
       default = all;
     };
   };
