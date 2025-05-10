69304efd674e21cd71319d0fca76121a79a2890e
Immelancholy <lenalowes0@gmail.com>
2025-05-10 14:42:19 +0100

    Update Neo to write shell application

--- packages/neo.nix
+++ packages/neo.nix
@@ -1,6 +1,19 @@
-{writeShellScriptBin, ...}:
-writeShellScriptBin "neo.sh" ''
-  kitty @ set-spacing padding=2
-  neo -a -S 20 -d 1 -f 144 -C "$XDG_CONFIG_HOME"/neo/colors -b 1 -m "Welcome, ''${USER^}." --lingerms=1,1 --rippct=0 "$@"
-  kitty @ set-spacing padding=default
-''
+{
+  writeShellApplication,
+  pkgs,
+  ...
+}:
+writeShellApplication {
+  name = "neo.sh";
+
+  runtimeInputs = [
+    pkgs.kitty
+    pkgs.neo
+  ];
+
+  text = ''
+    kitty @ set-spacing padding=2
+    neo -a -S 20 -d 1 -f 144 -C "$XDG_CONFIG_HOME"/neo/colors -b 1 -m "Welcome, ''${USER^}." --lingerms=1,1 --rippct=0 "$@"
+    kitty @ set-spacing padding=default
+  '';
+}
