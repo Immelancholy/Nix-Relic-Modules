4380e4f30fb0a916e9e67fe4ae5ec4b0dcfd762d
Immelancholy <lenalowes0@gmail.com>
2025-05-14 12:47:51 +0100

    Delete These

--- modules/home-manager/stylix/default.nix
+++ modules/home-manager/stylix/default.nix
@@ -1,13 +1,5 @@
 {
   imports = [
-    ./nixvim.nix
-    ./kitty.nix
-    ./yazi.nix
-    ./btop.nix
-    ./spicetify.nix
-    ./tmux.nix
     ./zenTarget.nix
-    ./zen.nix
-    ./vesktop.nix
   ];
 }
