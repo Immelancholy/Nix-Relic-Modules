d3362855c7b41dd03bfeeb20485f8788db7274a3
Immelancholy <lenalowes0@gmail.com>
2025-05-14 02:32:47 +0100

    Push this

--- packages/cliphist.nix
+++ packages/cliphist.nix
@@ -1,20 +1,2 @@
-{
-  writeShellApplication,
-  pkgs,
-  rofi-package ? pkgs.rofi-wayland,
-  wl-clip-package ? pkgs.wl-clipboard,
-  ...
-}:
-writeShellApplication {
-  name = "cliphist.sh";
-  runtimeInputs = [
-    pkgs.cliphist
-    rofi-package
-    wl-clip-package
-    pkgs.gawk
-  ];
-  text = ''
-    set +o nounset
-    ${builtins.readFile ./Bash/cliphist.sh}
-  '';
-}
+{writeShellScriptBin, ...}:
+writeShellScriptBin "cliphist.sh" (builtins.readFile ./Bash/cliphist.sh)
