862b1956a9246dd65dd4c777463dc13c3b1fb27c
Immelancholy <lenalowes0@gmail.com>
2025-05-22 04:29:21 +0100

    This

--- packages/neo.nix
+++ packages/neo.nix
@@ -12,7 +12,7 @@ writeShellApplication {
   ];
 
   text = ''
-    kitty @ set-spacing padding=2
+    kitty @ set-spacing padding=0
     neo -a -S 20 -d 1 -f 144 -C "$XDG_CONFIG_HOME"/neo/colors -b 1 -m "Welcome, ''${USER^}." --lingerms=1,1 --rippct=0 "$@"
     kitty @ set-spacing padding=default
   '';
