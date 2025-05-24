e305e64061b36b939a9f6156bb68368b73e99bf6
Immelancholy <lenalowes0@gmail.com>
2025-05-24 12:11:26 +0100

    This

--- packages/neo.nix
+++ packages/neo.nix
@@ -11,6 +11,6 @@ writeShellApplication {
   ];
 
   text = ''
-    neo -a -S 20 -d 1 -f $FRAMERATE -C "$XDG_CONFIG_HOME"/neo/colors -b 1 -m "Welcome, ''${USER^}." --lingerms=1,1 --rippct=0 "$@"
+    neo -a -S 20 -d 1 -f "$FRAMERATE" -C "$XDG_CONFIG_HOME"/neo/colors -b 1 -m "Welcome, ''${USER^}." --lingerms=1,1 --rippct=0 "$@"
   '';
 }
