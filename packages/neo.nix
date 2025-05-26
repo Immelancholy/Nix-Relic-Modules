12437ee5d31a570f894c8a23aec26938065f2e48
Immelancholy <lenalowes0@gmail.com>
2025-05-26 02:26:22 +0100

    Try this? Having some problems with ghostty

--- packages/neo.nix
+++ packages/neo.nix
@@ -11,6 +11,7 @@ writeShellApplication {
   ];
 
   text = ''
-    neo -a -S 20 -d 1 -f "$FRAMERATE" -C "$XDG_CONFIG_HOME"/neo/colors -b 1 -m "Welcome, ''${USER^}." --lingerms=1,1 --rippct=0 "$@"
+    user="''${USER^}"
+    neo -a -S 20 -d 1 -f "$FRAMERATE" -C "$XDG_CONFIG_HOME"/neo/colors -b 1 -m "Welcome, ''${user}." --lingerms=1,1 --rippct=0
   '';
 }
