bfeca6a77f2a825c50e6ec3f082300dc8744dc6a
Immelancholy <lenalowes0@gmail.com>
2025-06-04 14:28:31 +0100

    Add default xdg_screenshots_dir to ss

--- packages/Bash/ss.sh
+++ packages/Bash/ss.sh
@@ -37,6 +37,7 @@ post_cmd() {
 }
 
 confDir="${confDir:-$HOME/.config}"
+XDG_SCREENSHOTS_DIR="${XDG_SCREENSHOTS_DIR:-$HOME/Pictures/Screenshots}"
 save_dir="${XDG_SCREENSHOTS_DIR}"
 mkdir -p "$save_dir"
 save_file=$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')
