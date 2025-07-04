c4f554b335dcdae9cf3d40f6d332f9ddf757b8f2
Immelancholy <lenalowes0@gmail.com>
2025-07-04 18:25:16 +0100

    Update neo.nix

--- packages/neo.nix
+++ packages/neo.nix
@@ -12,6 +12,8 @@ writeShellApplication {
 
   text = ''
     user="''${USER^}"
+    kitty @ set-spacing padding=0
     neo -a -S 20 -d 1 -f "$FRAMERATE" -C "$XDG_CONFIG_HOME"/neo/colors -b 1 -m "Welcome, ''${user}." --lingerms=1,1 --rippct=0
+    kitty @ set-spacing padding=default
   '';
 }
