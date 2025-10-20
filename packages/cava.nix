d336e9f54b263bfa7fb2d0368000cd25038e10a2
Immelancholy <lenalowes0@gmail.com>
2025-10-20 21:33:57 +0100

    fix(cava): pass arguments to cava
    Ensures that any arguments passed to the shell script are also passed
    to the cava executable. This allows for customization of cava's
    behavior directly from the script invocation.

--- packages/cava.nix
+++ packages/cava.nix
@@ -14,7 +14,7 @@ writeShellApplication {
   text = ''
     kitty @ set-spacing padding=0
     kitty @ set-font-size 3
-    cava
+    cava "$@"
     kitty @ set-spacing padding=default
     kitty @ set-font-size 10
   '';
