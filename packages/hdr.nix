59a55732d538bc5feeaa1117527d7ebccec0b362
Immelancholy <lenalowes0@gmail.com>
2025-10-26 04:05:57 +0000

    fix(hdr.nix): improve monitor argument handling
    Adjusts the `force-hdr` script to correctly handle monitor arguments,
    allowing for dynamic HDR toggling on specific displays.
    
    - Passes monitor resolution and refresh rate when setting HDR.
    - Restores original monitor settings upon disabling HDR.

--- packages/hdr.nix
+++ packages/hdr.nix
@@ -1,12 +1,14 @@
 {writeShellScriptBin}:
 writeShellScriptBin "force-hdr" ''
   FORCE_HDR=$(hyprctl getoption render:cm_fs_passthrough | sed -n '1p' | awk '{print $2}')
+  MONITOR="$*"
+  MONITOR_HDR="$MONITOR%,*"
 
   if [ "$FORCE_HDR" != 1 ]; then
-  	hyprctl keyword monitor "$*, cm, hdr"
+  	hyprctl keyword monitor "$MONITOR_HDR, 10, cm, hdr"
   	hyprctl keyword render:cm_fs_passthrough 1
   else
-  	hyprctl keyword monitor "$*, cm, srgb"
+  	hyprctl keyword monitor "$MONITOR, cm, srgb"
   	hyprctl keyword render:cm_fs_passthrough 2
   fi
 ''
