9d48ccfaafcfe6f1df7dc45a912f82ade7d3239e
Immelancholy <lenalowes0@gmail.com>
2025-10-26 04:16:06 +0000

    fix(hdr.nix): simplify monitor parsing
    Replaces `cut` with bash parameter expansion for extracting the monitor name.

--- packages/hdr.nix
+++ packages/hdr.nix
@@ -2,7 +2,7 @@
 writeShellScriptBin "force-hdr" ''
   FORCE_HDR=$(hyprctl getoption render:cm_fs_passthrough | sed -n '1p' | awk '{print $2}')
   MONITOR="$*"
-  MONITOR_HDR=$(echo "$MONITOR" | cut -f1 -d",")
+  MONITOR_HDR="''${MONITOR%,*}"
 
   if [ "$FORCE_HDR" != 1 ]; then
   	hyprctl keyword monitor "$MONITOR_HDR, 10, cm, hdr"
